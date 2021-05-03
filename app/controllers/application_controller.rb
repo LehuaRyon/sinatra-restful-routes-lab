a2b3a7b0311e2250fe9a376df3f949b3f1f06bb4
Jasmine Ryon <jryon12@priorypride.net>
2021-05-03 10:29:34 -0700

    commit tree a5cff7243afa9501a3cee53a1c1a3ed0cc543f1c

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,48 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  # code actions here!
+  # gets new form
+  get '/recipes/new' do
+    erb :new
+  end
+
+
+  # gets all recipes from index pg
+  get '/recipes' do
+    @recipes = Recipe.all
+    erb :index
+  end
+
+  # gets recipe by id
+  get '/recipes/:id' do
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :show
+  end 
+
+  # gets edit form for recipe by id
+  get '/recipes/:id/edit' do
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :edit
+  end
+
+  # creates new recipe w/user input from form
+  post '/recipes' do
+    @recipe = Recipe.create(params)
+    redirect to "/recipes/#{@recipe.id}"
+  end 
+
+  # updates recipe w/user input from edit form
+  patch '/recipes/:id' do
+    @recipe = Recipe.find_by_id(params[:id])
+    @coffee.update(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
+    redirect "/recipes/#{@recipe.id}"
+  end
+
+  # deletes recipe by id
+  delete '/recipes/:id' do
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.delete
+    redirect to '/recipes'
+  end 
 
 end
